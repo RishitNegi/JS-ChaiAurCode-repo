69fba24c2e3afe27bd76babada972ac5289aa250
RishitNegi <rishitnegi2001@gmail.com>
2023-11-21 19:08:16 +0530

    Conversion Operations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,33 @@
+// let someNumber = 33;
+
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ************************************* OPERATIONS ********************************************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2*2);
+// console.log(2**2); //To the Power
+// console.log(2%2); //Modulus or Remainder
+
+let str1 = "Hello,";
+let str2 = " Rishit";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2); // Ans = 12
+// console.log(1 + "2"); // Ans = 12
+// console.log("1" + "2"); // Ans = 12
+// console.log("1" + 2 + 2); // Ans = 122
+// console.log(1 + 2 + "2"); // Ans = 32
+
+
+console.log(true);
