b80a3a9d722c30c4e4239c51936b26d97da0626d
RishitNegi <rishitnegi2001@gmail.com>
2023-11-21 23:18:44 +0530

    Datatype Notes

---
+++ 01_basics/03.1_datatypeNotes.js
@@ -0,0 +1,24 @@
+// Primitive
+
+// 7 Types:
+// String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const score = 100;
+const anotherScore = 100.3;
+
+const isLoggedIn = true;
+const outsideTemp = null;
+let userEmail = undefined;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+const bigInt = 9147639746; 
+
+
+// Reference (Non primitive)
+// Arrays, Objects, Functions
+
+// In order to master JavaScript, master Objects and Browser's web events 
