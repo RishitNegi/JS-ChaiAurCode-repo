9042e60673ed6fde81b4c4cbfaad01e2df0e04b8
RishitNegi <rishitnegi2001@gmail.com>
2023-11-22 07:58:02 +0530

    Stack and Heap memory

--- 01_basics/03.1_datatypeNotes.js
+++ 01_basics/03.1_datatypeNotes.js
@@ -22,3 +22,31 @@ const bigInt = 9147639746;
 // Arrays, Objects, Functions
 
 // In order to master JavaScript, master Objects and Browser's web events 
+
+
+
+// Stack(Primitive) and Heap(Non-primitive) Datatype
+
+let myInstagram = "Rishit_Singh_negi";
+let anotherName = myInstagram;
+anotherName = "Rishy_negi";
+
+// console.log(myInstagram);
+// console.log(anotherName);
+
+// In Primitive datatype(Stack), a copy is created for different variables and on changing the values of other variables, no changes are made in the original data.
+
+let userOne = {
+    email: "rishy@gmail.com",
+    upi: "userOne@ybl"
+}
+
+let userTwo = userOne;
+
+userTwo.email = "rishit@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+// In Non-primitive/Reference datatype(Heap), reference of the original data is given. Therefore, any changes that are made will change the original data.
+
